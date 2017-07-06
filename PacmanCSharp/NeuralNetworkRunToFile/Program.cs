bde0edb52c3601aac9e9f2c690978b3ab04da033
Mihail Morosan <me@morosanmihail.com>
2017-07-06 03:09:55 +0100

    Added utility to extract neural networks from runs.

---
+++ PacmanCSharp/NeuralNetworkRunToFile/Program.cs
@@ -0,0 +1,52 @@
+﻿using MMPac;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NeuralNetworkRunToFile
+{ 
+    class Program
+    {
+        public static string RunFolder = "";
+        public static string SaveFolder = "";
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                RunFolder = args[0];
+                SaveFolder = args[1];
+            }
+            else
+            {
+                Console.WriteLine("You have to supply 2 arguments: runFolder and saveFolder");
+                Console.ReadKey();
+                return;
+            }
+
+            var files = Directory.GetFiles(RunFolder);
+            var latestfile = files.OrderBy(s => s).Last();
+
+            var doc = new XmlDocument();
+            doc.Load(latestfile);
+            
+
+            var Node = doc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes[8].ChildNodes[0].ChildNodes[5];
+
+            List<double> Weights = new List<double>();
+
+            foreach(dynamic X in Node.ChildNodes)
+            {
+                Weights.Add(double.Parse(X.InnerText));
+            }
+
+            var NN = new MMLocPac(Weights);
+            NN.SaveWeights(Path.Combine(SaveFolder, "NeuralNetworkLocPac.nn"));
+        }
+    }
+}
